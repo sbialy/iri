e58a314e31ea0128dec85b7cf6a2c04340924883
Paul D Handy <paul.d.handy@gmail.com>
2017-05-30 08:20:02 -0600

    unsigned milestones

--- src/main/java/com/iota/iri/Milestone.java
+++ src/main/java/com/iota/iri/Milestone.java
@@ -155,7 +155,8 @@ public class Milestone {
                                         ISS.NUMBER_OF_FRAGMENT_CHUNKS),
                                 signatureFragmentTrits)),
                                 transactionViewModel2.trits(), 0, index, NUMBER_OF_KEYS_IN_A_MILESTONE);
-                        if (testnet || (new Hash(merkleRoot)).equals(coordinator)) {
+                        //if (testnet || (new Hash(merkleRoot)).equals(coordinator)) {
+                        {
                             new MilestoneViewModel(index, transactionViewModel.getHash()).store(tangle);
                             return true;
                         }
