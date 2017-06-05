c2a046b9972ebb6bc57af179e8df327ed8654dd3
Paul Handy <paul@pauldhandy.com>
2017-06-05 10:49:34 -0700

    Updated iri.rb

--- iri.rb
+++ iri.rb
@@ -1,17 +1,15 @@
 class IRI < Formula
     desc "IOTA Reference Implementation"
     homepage "https://github.com/iotaledger/iri"
-
-    resource "iri" do
-        url "https://github.com/iotaledger/iri/releases/download/1.1.4.1/iri-1.1.4.2.jar"
-        sha256 "f441ca3c8992c4ed1e2c19c6b9080d22ad72fa056ab10dec0a4b7535833f1a47"
-    end
+    url "https://github.com/iotaledger/iri/releases/download/1.1.4.2/iri-1.1.4.2.jar"
+    sha256 "f441ca3c8992c4ed1e2c19c6b9080d22ad72fa056ab10dec0a4b7535833f1a47"
+    //depends_on "mvn" => :build
 
     bottle :unneeded
 
     def install
-        prefix.install "iri-1.1.4.1.jar"
-        bin.install "iri"
+        //prefix.install "iri-1.1.4.2.jar"
+        bin.install "iri-1.1.4.2.jar"
     end
 
     /*
